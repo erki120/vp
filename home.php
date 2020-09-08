738ad65abfb2d89018c89f02b163ef01b8a96052
Erki Pahovski <erki120@users.noreply.github.com>
2020-09-08 11:09:22 +0300

    Esimene üleslaadimine

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+	$username = "Erki Pahovski";
+	$fulltimenow = date("d.m.Y H:i:s");
+	$hournow = date("H");
+	$partofday = "lihtsalt aeg";
+	if($hournow < 6){
+		$partofday = "uneaeg";
+	}// enne 6
+	if($hournow >= 8 and $hournow <= 18){
+		$partofday = "õppimise aeg";
+	}
+?>
+
+<!DOCTYPE HTML>
+
+<html lang="et">
+
+<head>
+<meta charset="utf-8">
+<title><?php echo $username; ?> veebileht</title>
+</head>
+
+<body>
+
+<h1>Erki Pahovski</h1>
+
+<p><?php echo $fulltimenow; ?></p>
+
+<p><?php echo "Praegu on ".$partofday."."; ?></p>
+
+
+</body>
+
+</html>
